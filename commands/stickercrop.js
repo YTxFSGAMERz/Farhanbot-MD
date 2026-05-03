92c6f5fb7ca38a4a92eb878d8be41f4f0e97f7ee
Farhan <94067297+YTxFSGAMERz@users.noreply.github.com>
2026-05-03 20:38:04 +0530

    Update stickercrop.js

--- commands/stickercrop.js
+++ commands/stickercrop.js
@@ -37,7 +37,7 @@ async function stickercropCommand(sock, chatId, message) {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '',
-                    newsletterName: 'Farhanbot MD',
+                    newsletterName: 'FarhanBot MD',
                     serverMessageId: -1
                 }
             }
@@ -59,7 +59,7 @@ async function stickercropCommand(sock, chatId, message) {
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: '',
-                        newsletterName: 'Farhanbot MD',
+                        newsletterName: 'FarhanBot MD',
                         serverMessageId: -1
                     }
                 }
@@ -149,7 +149,7 @@ async function stickercropCommand(sock, chatId, message) {
         // Create metadata
         const json = {
             'sticker-pack-id': crypto.randomBytes(32).toString('hex'),
-            'sticker-pack-name': settings.packname || 'Farhanbot',
+            'sticker-pack-name': settings.packname || 'FSGAMERz',
             'emojis': ['✂️']
         };
 
@@ -187,7 +187,7 @@ async function stickercropCommand(sock, chatId, message) {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '',
-                    newsletterName: 'Farhanbot MD',
+                    newsletterName: 'FarhanBot MD',
                     serverMessageId: -1
                 }
             }
@@ -235,7 +235,7 @@ async function stickercropFromBuffer(inputBuffer, isAnimated) {
     await img.load(webpBuffer);
     const json = {
         'sticker-pack-id': crypto.randomBytes(32).toString('hex'),
-        'sticker-pack-name': settings.packname || 'Farhanbot',
+        'sticker-pack-name': settings.packname || 'FSGAMERz',
         'emojis': ['✂️']
     };
     const exifAttr = Buffer.from([0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x00, 0x00, 0x00]);
