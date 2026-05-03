cef60d1a461b379bce0de099113da252075aa039
Farhan <94067297+YTxFSGAMERz@users.noreply.github.com>
2026-05-03 20:37:50 +0530

    Update img-blur.js

--- commands/img-blur.js
+++ commands/img-blur.js
@@ -66,7 +66,7 @@ async function blurCommand(sock, chatId, message, quotedMessage) {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '',
-                    newsletterName: 'Farhanbot MD',
+                    newsletterName: 'FarhanBot MD',
                     serverMessageId: -1
                 }
             }
