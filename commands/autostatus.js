106b8da489fe2fe9b0295880183115a16298a435
Farhan <94067297+YTxFSGAMERz@users.noreply.github.com>
2026-05-03 20:38:56 +0530

    Update autostatus.js

--- commands/autostatus.js
+++ commands/autostatus.js
@@ -8,7 +8,7 @@ const channelInfo = {
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
             newsletterJid: '',
-            newsletterName: 'Farhanbot MD',
+            newsletterName: 'FarhanBot MD',
             serverMessageId: -1
         }
     }
