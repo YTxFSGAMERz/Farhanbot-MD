a80947bb25e9781d976460f097c82b68ae237165
Farhan <94067297+YTxFSGAMERz@users.noreply.github.com>
2026-05-03 20:38:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
-  packname: 'Farhanbot-MD',
-  author: 'Farhan',
-  botName: "Farhanbot-MD",
-  botOwner: 'Farhan', // Your name
+  packname: 'FarhanBot-MD',
+  author: 'FSGAMERz',
+  botName: "FarhanBot-MD",
+  botOwner: 'FSGAMERz', // Your name
   ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
